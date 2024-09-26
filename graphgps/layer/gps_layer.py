2edfc2bf4d7c70ef4e0616324c30b7bf4d76f911
Omer Talmi <43521606+omertalmi5@users.noreply.github.com>
2024-09-26 10:16:51 +0300

    Change residual

--- graphgps/layer/gps_layer.py
+++ graphgps/layer/gps_layer.py
@@ -265,7 +265,7 @@ class GPSLayer(nn.Module):
             attn_output = attn_output[mask]
 
             # Residual connection
-            h = h_in1 + attn_output
+            h = h_local_dense + attn_output
 
             # Optional normalization and dropout
             if self.layer_norm:
